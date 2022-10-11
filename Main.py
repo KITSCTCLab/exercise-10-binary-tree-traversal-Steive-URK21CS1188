40ae1bf475f6869a1eef0937f8f0242e3d437e3f
Steive-URK21CS1188 <110010600+Steive-URK21CS1188@users.noreply.github.com>
2022-10-11 12:09:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,47 +3,29 @@ class BinaryTreeNode:
         self.data = data
         self.left_child = None
         self.right_child = None
-     def insert(root, data):
-        """If binary search tree is empty, make a new node, declare it as root and return the root.
-            If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-            If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-            Finally, return the root.
-            """
-        # Write your code here
-        if self.data is not None:
-            if data < self.data:
-                if self.data is None:
-                    self.left_child = BinaryTreeNode(data)
-                else:
-                    self.left_child.insert(root, data)
-            elif data > self.data:
-                if self.data is None:
-                    self.right_child = BinaryTreeNode(data)
-                else:
-                    self.right_child.insert(root, data)
-        else:
-            self.data=data
+   
     
-# def insert(root, data) -> BinaryTreeNode:
-#     """If binary search tree is empty, make a new node, declare it as root and return the root.
-#         If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-#         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-#         Finally, return the root.
-#         """
-#     # Write your code here
-#     if self.data is not None:
-#         if data < self.data:
-#             if self.data is None:
-#                 self.left_child = BinaryTreeNode(data)
-#             else:
-#                 self.left_child.insert(root, data)
-#         elif data > self.data:
-#             if self.data is None:
-#                 self.right_child = BinaryTreeNode(data)
-#             else:
-#                 self.right_child.insert(root, data)
-#     else:
-#         self.data=data
+def insert(root, data) -> BinaryTreeNode:
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
+    # Write your code here
+    if self.data is not None:
+        if data < self.data:
+            if self.data is None:
+                self.left_child = BinaryTreeNode(data)
+            else:
+                self.left_child.insert(root, data)
+        elif data > self.data:
+            if self.data is None:
+                self.right_child = BinaryTreeNode(data)
+            else:
+                self.right_child.insert(root, data)
+    else:
+        self.data=data
+          
 def inorder(root) -> None:
     # Write your code here
     res = []
